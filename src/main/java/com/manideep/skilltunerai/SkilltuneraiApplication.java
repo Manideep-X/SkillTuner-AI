d10202589af818fc474d7bd8cd35fe7f94e71720
Manideep <deepanddipyo@gmail.com>
2025-10-08 00:37:03 +0530

    initial commit

---
+++ src/main/java/com/manideep/skilltunerai/SkilltuneraiApplication.java
@@ -0,0 +1,13 @@
+package com.manideep.skilltunerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilltuneraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilltuneraiApplication.class, args);
+	}
+
+}
