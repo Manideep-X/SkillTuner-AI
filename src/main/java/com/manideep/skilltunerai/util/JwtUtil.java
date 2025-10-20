00726b4dd990bf5f5e61e87c54113995c0dc2972
Manideep <deepanddipyo@gmail.com>
2025-10-21 00:31:47 +0530

    JWT request filter and it's util classes are added for token generation and validation

---
+++ src/main/java/com/manideep/skilltunerai/util/JwtUtil.java
@@ -0,0 +1,59 @@
+package com.manideep.skilltunerai.util;
+
+import java.util.Date;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private final String secretJWTKey;
+    private final int expiryDuration = 1000*60*60*3; // 3 hours validity
+
+    public JwtUtil(@Value("${jwt.secret}") String secretJWTKey) {
+        this.secretJWTKey = secretJWTKey;
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretJWTKey.getBytes());
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String getUsername(String token) {
+        return getAllClaims(token).getSubject();
+    }
+
+    public Boolean isTokenExpired(String token) {
+        return getAllClaims(token).getExpiration().before(new Date());
+    }
+
+    // First need to check if the token's username matches with the userDetails, then if the token expired.
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
+        return (getUsername(token).equals(userDetails.getUsername()) && isTokenExpired(token));
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiryDuration))
+                .signWith(getSigningKey()) // For signature we need to sign the secret key with HS256
+                .compact(); // This generate the final JWT token
+    }
+
+}
