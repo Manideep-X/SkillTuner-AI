1514d551f283887ce520a34432a8fb25b06f7d6e
Manideep <deepanddipyo@gmail.com>
2025-10-21 00:33:08 +0530

    simple controller with configuration package is added

---
+++ src/main/java/com/manideep/skilltunerai/config/SecurityConfig.java
@@ -0,0 +1,5 @@
+package com.manideep.skilltunerai.config;
+
+public class SecurityConfig {
+
+}
---
+++ src/main/java/com/manideep/skilltunerai/controller/SimpleController.java
@@ -0,0 +1,23 @@
+package com.manideep.skilltunerai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping({"/health", "/info", "/status", "/checkup"})
+public class SimpleController {
+
+    @GetMapping
+    public ResponseEntity<Map<String, String>> appHealth() {
+        return ResponseEntity.ok(Map.of(
+            "message", "This website is now up and running!"
+        ));
+    }
+
+}
