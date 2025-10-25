8aa1c4c865163d21064a9fbe6a9e2097f45a3340
Manideep <deepanddipyo@gmail.com>
2025-10-26 00:21:47 +0530

    JPA relationship added between resume and users entity along with helper methods

--- src/main/java/com/manideep/skilltunerai/entity/Resume.java
+++ src/main/java/com/manideep/skilltunerai/entity/Resume.java
@@ -1,10 +1,18 @@
 package com.manideep.skilltunerai.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,12 +22,24 @@ public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    
+    @Column(name = "resume_url", nullable = false)
+    private String resumeUrl;
 
     @Column(nullable = false)
     private String content;
 
-    public Resume(long id, String content) {
-        this.id = id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users user;
+
+    @OneToMany(mappedBy = "resume", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<JobDescription> jobDescriptions = new ArrayList<>();
+
+    public Resume() {
+    }
+    public Resume(String resumeUrl, String content) {
+        this.resumeUrl = resumeUrl;
         this.content = content;
     }
 
@@ -27,11 +47,42 @@ public class Resume {
         return id;
     }
 
+    public String getResumeUrl() {
+        return resumeUrl;
+    }
+    public void setResumeUrl(String resumeUrl) {
+        this.resumeUrl = resumeUrl;
+    }
+    
     public String getContent() {
         return content;
     }
     public void setContent(String content) {
         this.content = content;
+    }
+    
+    public Users getUser() {
+        return user;
+    }
+    public void setUser(Users user) {
+        this.user = user;
+    }
+
+    public List<JobDescription> getJobDescriptions() {
+        return jobDescriptions;
+    }
+    public void setJobDescriptions(List<JobDescription> jobDescriptions) {
+        this.jobDescriptions = jobDescriptions;
+    }
+
+    // Helper methods
+    public void addJD(JobDescription job) {
+        jobDescriptions.add(job);
+        job.setResume(this);
+    }
+    public void removeJD(JobDescription job) {
+        jobDescriptions.remove(job);
+        job.setResume(null);
     }
 
 }
