95af40657679dcc163a8abe30d3b1c56f19da057
Manideep <deepanddipyo@gmail.com>
2025-10-27 23:09:18 +0530

    Authentication service layer's interface is added with abstract methods for signin and signup

---
+++ src/main/java/com/manideep/skilltunerai/service/AuthServiceImpl.java
@@ -0,0 +1,26 @@
+package com.manideep.skilltunerai.service;
+
+import org.springframework.stereotype.Service;
+
+import com.manideep.skilltunerai.dto.SigninRequestDTO;
+import com.manideep.skilltunerai.dto.SigninResponseDTO;
+import com.manideep.skilltunerai.dto.SignupRequestDTO;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    
+
+    @Override
+    public void signup(SignupRequestDTO signupRequestDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'signup'");
+    }
+
+    @Override
+    public SigninResponseDTO signin(SigninRequestDTO signinRequestDTO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'signin'");
+    }
+
+}
