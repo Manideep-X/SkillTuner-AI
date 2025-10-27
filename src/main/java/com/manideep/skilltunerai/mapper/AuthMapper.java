b52b29d4122820c3162f0bd459aabe8a48c8b315
Manideep <deepanddipyo@gmail.com>
2025-10-27 23:11:52 +0530

    DTOs are added for authentication request and response, along with authentication mappers

---
+++ src/main/java/com/manideep/skilltunerai/mapper/AuthMapper.java
@@ -0,0 +1,50 @@
+package com.manideep.skilltunerai.mapper;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.manideep.skilltunerai.dto.SigninResponseDTO;
+import com.manideep.skilltunerai.dto.SignupRequestDTO;
+import com.manideep.skilltunerai.entity.Users;
+
+@Component
+public class AuthMapper {
+
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthMapper(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public Users signupReqToUsersObj(SignupRequestDTO signupRequestDTO) {
+
+        Users users = new Users();
+
+        users.setFirstName(signupRequestDTO.getFirstName());
+        users.setLastName(signupRequestDTO.getLastName());
+        users.setEmail(signupRequestDTO.getEmail());
+        users.setPassword(passwordEncoder.encode(signupRequestDTO.getPassword()));
+        users.setActiveResumeId(null);
+
+        return users;
+
+    }
+
+    public SigninResponseDTO usersObjToSigninRes(Users users, String jwtToken) {
+
+        SigninResponseDTO signinResponseDTO = new SigninResponseDTO();
+
+        signinResponseDTO.setId(users.getId());
+        signinResponseDTO.setFirstName(users.getFirstName());
+        signinResponseDTO.setLastName(users.getLastName());
+        signinResponseDTO.setEmail(users.getEmail());
+        signinResponseDTO.setActiveResumeId(users.getActiveResumeId());
+        signinResponseDTO.setCreationTime(users.getCreationTime());
+        signinResponseDTO.setUpdationTime(users.getUpdationTime());
+        signinResponseDTO.setJwtToken(jwtToken);
+
+        return signinResponseDTO;
+
+    }
+
+}
