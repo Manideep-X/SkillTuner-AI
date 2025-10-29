4789a79c30ec616239f7d029fd422702b55e7587
Manideep <deepanddipyo@gmail.com>
2025-10-29 22:50:01 +0530

    new endpoints in controller and authentication service layer added for signin and signup

--- src/main/java/com/manideep/skilltunerai/service/AuthService.java
+++ src/main/java/com/manideep/skilltunerai/service/AuthService.java
@@ -1,15 +1,28 @@
 package com.manideep.skilltunerai.service;
 
+import org.springframework.security.authentication.BadCredentialsException;
+
 import com.manideep.skilltunerai.dto.SigninRequestDTO;
 import com.manideep.skilltunerai.dto.SigninResponseDTO;
 import com.manideep.skilltunerai.dto.SignupRequestDTO;
+import com.manideep.skilltunerai.entity.Users;
+import com.manideep.skilltunerai.exception.DuplicateValueException;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.PersistenceException;
 
 public interface AuthService {
 
     // Method for registering new user
-    void signup(SignupRequestDTO signupRequestDTO);
+    void signup(SignupRequestDTO signupRequestDTO) throws DuplicateValueException, PersistenceException;
     
     // Method for signing in exsisting user
-    SigninResponseDTO signin(SigninRequestDTO signinRequestDTO);
+    SigninResponseDTO signin(SigninRequestDTO signinRequestDTO) throws BadCredentialsException;
+
+    // Method for getting the user entity who is currently signed-in
+    Users currentlyLoggedinUser();
+
+    // Method for getting the user entity by its email address
+    Users usersEntityByItsEmail(String email) throws EntityNotFoundException;
 
 }
