04a9fe0d27b46a744c5f4c349641904aadda45f0
Manideep <deepanddipyo@gmail.com>
2025-10-31 23:53:59 +0530

    New exception class is added for parsing error, and global exception handler is modified

--- src/main/java/com/manideep/skilltunerai/exception/GlobalExceptionHandler.java
+++ src/main/java/com/manideep/skilltunerai/exception/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import io.jsonwebtoken.io.IOException;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceException;
 
@@ -40,35 +41,64 @@ public class GlobalExceptionHandler {
             
     }
 
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<Map<String, String>> handleAccessDenied(SecurityException exception) {
+        
+        logger.error("Access denied to use this operation: ", exception);
+        // HTTP status code: 403
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(Map.of(
+            "message", exception.getMessage()
+        ));
+            
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Map<String, String>> handleUserNotFound(EntityNotFoundException exception) {
 
-        logger.warn("User not found: ", exception);
+        logger.warn("Data not found: ", exception);
         // HTTP status code: 404
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of(
             "message", exception.getMessage()
         ));
 
     }
+    
+    @ExceptionHandler({FileLoadingException.class, IllegalArgumentException.class})
+    public ResponseEntity<Map<String, String>> handleCorruptedFileLoaded(Exception exception) {
 
-    @ExceptionHandler(PersistenceException.class)
-    public ResponseEntity<Map<String, String>> handleErrorWhileSavingInDB(PersistenceException exception) {
-
-        logger.error("JPA pesistence error: ", exception);
-        // HTTP status code: 500
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+        logger.warn("Wrong extention or corrupted file: ", exception);
+        // HTTP status code: 406
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of(
             "message", exception.getMessage()
         ));
 
     }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleOtherExceptions(Exception exception) {
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<Map<String, String>> handleIOException(IOException exception) {
+
+        logger.error("Unable to read the file: ", exception);
+        // HTTP status code: 400
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of(
+            "message", exception.getMessage()
+        ));
+
+    }
+
+    @ExceptionHandler({PersistenceException.class, Exception.class})
+    public ResponseEntity<Map<String, String>> handleErrorWhileSavingInDB(Exception exception) {
+
+        // HTTP status code: 500
+        if (exception instanceof PersistenceException) {
+            logger.error("JPA pesistence error: ", exception);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
+                "message", exception.getMessage()
+            ));
+        }
 
         logger.error("Unexpected error: ", exception);
-        // HTTP status code: 500
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of(
-            "message", "Our server faced an unexcepted error! Please try again later"
+            "message", "Our server faced an unexpected error! Please try again later"
         ));
 
     }
