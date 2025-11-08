ea0c96a413cda23fdf93a95076c96296243c2c5e
Manideep <deepanddipyo@gmail.com>
2025-11-08 23:58:24 +0530

    resume and job description's controllers are added along with cloud file exception for service layer

---
+++ src/main/java/com/manideep/skilltunerai/controller/JobDescriptionController.java
@@ -0,0 +1,54 @@
+package com.manideep.skilltunerai.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manideep.skilltunerai.dto.JobDesRequestDTO;
+import com.manideep.skilltunerai.dto.JobDesResponseDTO;
+import com.manideep.skilltunerai.service.JobDesService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("resume/{resumeId}/job-description")
+public class JobDescriptionController {
+
+    private final JobDesService jobDesService;
+
+    public JobDescriptionController(JobDesService jobDesService) {
+        this.jobDesService = jobDesService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveJobDescriptionForm(@RequestBody JobDesRequestDTO jobDesRequestDTO) {
+        jobDesService.saveJobDescription(jobDesRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+    
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobDesResponseDTO> getJDById(@PathVariable("id") long jdId, @PathVariable long resumeId) {
+        return ResponseEntity.ok(jobDesService.getJdDTOIfLinkedWithResume(jdId, resumeId));
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<String>> getMethodName(@PathVariable long resumeId) {
+        return ResponseEntity.ok(jobDesService.getsJobTitlesByResumeId(resumeId));
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteJDById(@PathVariable("id") long jdId, @PathVariable long resumeId) {
+        jobDesService.deleteJDByItsId(jdId, resumeId);
+        return ResponseEntity.ok(null);
+    }
+
+}
---
+++ src/main/java/com/manideep/skilltunerai/controller/ResumeController.java
@@ -0,0 +1,53 @@
+package com.manideep.skilltunerai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manideep.skilltunerai.dto.ResumeRequestDTO;
+import com.manideep.skilltunerai.dto.ResumeResponseDTO;
+import com.manideep.skilltunerai.service.ResumeService;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/resume")
+public class ResumeController {
+
+    private final ResumeService resumeService;
+
+    public ResumeController(ResumeService resumeService) {
+        this.resumeService = resumeService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewResume(@RequestBody ResumeRequestDTO resumeRequestDTO) {
+        resumeService.saveResumeAndUpload(resumeRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+    
+
+    @GetMapping("/{resume-id}")
+    public ResponseEntity<ResumeResponseDTO> getResumeDetails(@PathVariable("resume-id") long id) {
+        return ResponseEntity.ok(resumeService.getResumeResponseDTO(id));
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<ResumeResponseDTO>> getAllResumesOfCurrUser() {
+        return ResponseEntity.ok(resumeService.getResumeDTOsOfCurrUser());
+    }
+    
+    @DeleteMapping("/{resume-id}")
+    public ResponseEntity<Void> deleteResumeById(@PathVariable("resume-id") long id) {
+        resumeService.deleteAResume(id);
+        return ResponseEntity.ok(null);
+    }
+
+}
