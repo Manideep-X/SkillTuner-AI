76642d48b0a937f7f75c3a4ba67dbf3f3393d0a3
Manideep <deepanddipyo@gmail.com>
2025-11-15 00:00:49 +0530

    minor changes in endpoint of resume controller and redundant JPA methods are removed

--- src/main/java/com/manideep/skilltunerai/repository/JobDesRepository.java
+++ src/main/java/com/manideep/skilltunerai/repository/JobDesRepository.java
@@ -3,8 +3,6 @@ package com.manideep.skilltunerai.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.manideep.skilltunerai.entity.JobDescription;
 
@@ -13,9 +11,5 @@ public interface JobDesRepository extends JpaRepository<JobDescription, Long> {
     // Fetch all job descriptions of a resume
     // Internal query: select * from job_description_table where resume_id = ?1;
     List<JobDescription> findByResume_Id(long resumeId);
-
-    // Fetches all job titles of a resume
-    @Query("SELECT jd.jobTitle FROM JobDescription jd WHERE jd.resume.id = :resumeId")
-    List<String> findAllJobTitlesByResume_Id(@Param("resumeId") long resumeId);
 
 }
