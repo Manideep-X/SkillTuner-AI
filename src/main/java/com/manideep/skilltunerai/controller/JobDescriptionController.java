f74462587f7dad626d5b59597ff000905f97d0bb
Manideep <deepanddipyo@gmail.com>
2025-11-14 23:51:18 +0530

    parameter of the method to save job description is changed

--- src/main/java/com/manideep/skilltunerai/controller/JobDescriptionController.java
+++ src/main/java/com/manideep/skilltunerai/controller/JobDescriptionController.java
@@ -6,8 +6,6 @@ import com.manideep.skilltunerai.dto.JobDesRequestDTO;
 import com.manideep.skilltunerai.dto.JobDesResponseDTO;
 import com.manideep.skilltunerai.service.JobDesService;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,9 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 @RestController
-@RequestMapping("resume/{resumeId}/job-description")
+@RequestMapping("resumes/{resumeId}/job-descriptions")
 public class JobDescriptionController {
 
     private final JobDesService jobDesService;
@@ -29,26 +26,20 @@ public class JobDescriptionController {
     }
 
     @PostMapping
-    public ResponseEntity<JobDesResponseDTO> saveJobDescriptionForm(@RequestBody JobDesRequestDTO jobDesRequestDTO) {
+    public ResponseEntity<JobDesResponseDTO> createJobDescription(@RequestBody JobDesRequestDTO jobDesRequestDTO, @PathVariable long resumeId) {
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(jobDesService.saveJobDescription(jobDesRequestDTO));
+            .body(jobDesService.saveJobDescription(jobDesRequestDTO, resumeId));
     }
-    
 
     @GetMapping("/{id}")
-    public ResponseEntity<JobDesResponseDTO> getJDById(@PathVariable("id") long jdId, @PathVariable long resumeId) {
+    public ResponseEntity<JobDesResponseDTO> getJobDescriptionById(@PathVariable("id") long jdId, @PathVariable long resumeId) {
         return ResponseEntity.ok(jobDesService.getJdDTOIfLinkedWithResume(jdId, resumeId));
     }
     
-    @GetMapping
-    public ResponseEntity<List<String>> getJDTitles(@PathVariable long resumeId) {
-        return ResponseEntity.ok(jobDesService.getsJobTitlesByResumeId(resumeId));
-    }
-    
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteJDById(@PathVariable("id") long jdId, @PathVariable long resumeId) {
+    public ResponseEntity<Void> deleteJobDescriptionById(@PathVariable("id") long jdId, @PathVariable long resumeId) {
         jobDesService.deleteJDByItsId(jdId, resumeId);
-        return ResponseEntity.ok(null);
+        return ResponseEntity.noContent().build();
     }
 
 }
